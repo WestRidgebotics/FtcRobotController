bc1d1ceec6853de0ca92bbcd018a7962e9218343
Peri, Kamesh (External) <Kamesh_Peri@csx.com>
2022-01-19 19:28:52 -0600

    Did some logic

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/paths/right/RightPositionOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.auto.paths.right;
+
+import static org.firstinspires.ftc.teamcode.common.utils.DriveUtils.encoderDrive;
+
+import org.firstinspires.ftc.teamcode.auto.paths.BasePathAutoOpMode;
+
+/**
+ * This class should be extended by all classes that are posititioned in the right side.
+ */
+public abstract class RightPositionOpMode extends BasePathAutoOpMode {
+    /**
+     * {@inheritDoc}
+     */
+    protected void moveToShippingHub(char direction) {
+
+        if (level == 0 || level == -1 ) {
+        } else if (level == 1) {
+            driveUtils.encoderClaw(this, 0.5, -1300, 7);
+
+        } else if (level == 2) {
+            driveUtils.encoderClaw(this,0.5, -3900, 7);
+        }
+        if (level == 0 || level == 1 ) {
+            encoderDrive(this, 0.5, 10, 10, 5);
+            robot.turnLeft(this,35, 0.1);
+            encoderDrive(this, 0.5, 28, 28, 5);
+        } else if (level == 2 || level == -1){
+            safeRoute();
+        }
+
+
+    }
+    protected int getLevel() {
+        return level != -1 ? Math.abs(2 - level) : -1;
+    }
+}
